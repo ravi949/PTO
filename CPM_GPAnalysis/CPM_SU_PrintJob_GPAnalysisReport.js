5f97f7cb11a85b9c561663e4c148b1a7f9630a0d
ASAV Prasad Adari <PrasadA@amzurtech.com>
2017-08-17 21:44:11 +0530

    prasad:- optimized GPAnalysis scripts in version 2.0

--- CPM_GPAnalysis/CPM_SU_PrintJob_GPAnalysisReport.js
+++ CPM_GPAnalysis/CPM_SU_PrintJob_GPAnalysisReport.js
@@ -37,6 +37,7 @@ function(runtime, record, render, search, file) {
     				id		: jobId
     			});
     			
+    			//Loading template file
     			var templateFile = file.load({
     			    id : templateFileId
     			});
@@ -50,103 +51,20 @@ function(runtime, record, render, search, file) {
     			    record		 : jobRec
     			});
     			
-    			//===== Logic to combine both 'income' and 'cogs' searches and to pass the same to renderer object =====
-    			//Fetching all results from income search
-    			var incomeSearchObj = search.load({
-    			    id: incomeSearchId
-    			});
-    			
-    			incomeSearchObj.filters.push(search.createFilter({
-    			    name: 'custcoljobnbr',
-    			    operator: search.Operator.ANYOF,
-    			    values : jobId
-    			}));
-    			
-    			var incomeSearchResults = incomeSearchObj.run().getRange({
-	    			start: 0,
-	    			end : 999
-	    		})
-
-	    		//converting all income search results into required renderer data source
-	    		var incomeResults = [];
-
-    			if(incomeSearchResults)
-    			{
-    				log.debug('incomeSearchResults.length',incomeSearchResults.length);			
-    				for (var iTemp =0; iTemp < incomeSearchResults.length; iTemp++)
-    				{
-    			        var incomesource = {};
-    			        var incomeloopsource = {}; 
-    			        
-    			        incomesource.account = incomeSearchResults[iTemp].getText({ name: 'account', summary: search.Summary.GROUP });
-    			        incomesource.type = incomeSearchResults[iTemp].getText({ name: 'type', summary: search.Summary.GROUP });
-    			        incomesource.tranid = incomeSearchResults[iTemp].getValue({ name: 'tranid', summary: search.Summary.GROUP });
-    			        incomesource.trandate = incomeSearchResults[iTemp].getValue({ name: 'trandate', summary: search.Summary.MAX });
-    			        incomesource.mainname = incomeSearchResults[iTemp].getText({ name: 'mainname', summary: search.Summary.GROUP }); 
-    			        incomesource.memo = incomeSearchResults[iTemp].getValue({ name: 'memo', summary: search.Summary.GROUP });
-    			        incomesource.amount = (incomeSearchResults[iTemp].getValue({ name: 'amount', summary: search.Summary.SUM })).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-    			        incomeloopsource.values = incomesource;
-    			        incomeResults.push(incomeloopsource);
-    				}
-    			}
-	    		
-    			//Fetching all results from cogs search   		
-	    		var cogsSearchObj = search.load({
-    			    id: cogsSearchId
-    			});
-    			
-    			cogsSearchObj.filters.push(search.createFilter({
-    			    name: 'custcoljobnbr',
-    			    operator: search.Operator.ANYOF,
-    			    values : jobId
-    			}));
-    			
-    			var cogsSearchResults = cogsSearchObj.run().getRange({
-	    			start: 0,
-	    			end : 999
-	    		})
-    			
-	    		//converting all cogs search results into required renderer data source
-	    		var cogsResults = [];
-
-    			if(cogsSearchResults)
-    			{
-    				log.debug('cogsSearchResults.length',cogsSearchResults.length);			
-    				for (var jTemp =0; jTemp < cogsSearchResults.length; jTemp++)
-    				{
-    			        var cogssource = {};
-    			        var cogsloopsource = {};
-    			        
-    			        cogssource.account = cogsSearchResults[jTemp].getText({ name: 'account', summary: search.Summary.GROUP });
-    			        cogssource.type = cogsSearchResults[jTemp].getText({ name: 'type', summary: search.Summary.GROUP });
-    			        cogssource.tranid = cogsSearchResults[jTemp].getValue({ name: 'tranid', summary: search.Summary.GROUP });
-    			        cogssource.trandate = cogsSearchResults[jTemp].getValue({ name: 'trandate', summary: search.Summary.MAX });
-    			        cogssource.mainname = cogsSearchResults[jTemp].getText({ name: 'mainname', summary: search.Summary.GROUP }); 
-    			        cogssource.memo = cogsSearchResults[jTemp].getValue({ name: 'memo', summary: search.Summary.GROUP });
-    			        cogssource.amount = (cogsSearchResults[jTemp].getValue({ name: 'amount', summary: search.Summary.SUM })).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-    			        cogsloopsource.values = cogssource;
-    			        cogsResults.push(cogsloopsource);
-    				}
-    			}
-    			
     			//attaching the converted custom data source income search results to renderer 
     			renderer.addCustomDataSource({
     			    format: render.DataSource.OBJECT,
     			    alias: 'income',
-    			    data: {type : 'income', list : incomeResults }
+    			    data: {type : 'income', list : getRenderDataFromSearch(incomeSearchId, jobId) }
     			});
     			
     			//attaching the converted custom data source cogs search results to renderer
     			renderer.addCustomDataSource({
     			    format: render.DataSource.OBJECT,
     			    alias: 'cogs',
-    			    data: {type : 'cogs', list : cogsResults}
+    			    data: {type : 'cogs', list : getRenderDataFromSearch(cogsSearchId, jobId)}
     			});
-//    			log.debug('incomeResults', incomeResults);
-//    			log.debug('cogsResults', cogsResults);
-    			//============================================= END combine logic ======================================	    
+    				    
     			
     			xmlOutput = renderer.renderAsString();
     			
@@ -161,7 +79,63 @@ function(runtime, record, render, search, file) {
     		}
     }
     
+    /**
+     * To fetch the results from search and convert that into the desired renderer custom data source
+     * 
+     * @param {string} searchID
+     * @param {string} jobID
+     * 
+     * @return {Array} finalResults
+     */
+    function getRenderDataFromSearch(searchID, jobID){
+    	try{
+    		var finalResults = [];
+    		
+    		//loading the desired search
+    		var searchObj = search.load({
+    		    id: searchID
+    		});
+    		
+    		//Adding additional filter i.e. jobID to filter out the results by PrintJob id
+    		searchObj.filters.push(search.createFilter({
+    		    name: 'custcoljobnbr',
+    		    operator: search.Operator.ANYOF,
+    		    values : jobID
+    		}));
+    		
+    		var searchResults = searchObj.run().getRange({
+    			start: 0,
+    			end : 999
+    		})
+    		
+    		//converting all search results into required renderer data source format
+    		if(searchResults)
+    		{
+    			log.debug('searchResults.length',searchResults.length);			
+    			for (var iTemp =0; iTemp < searchResults.length; iTemp++)
+    			{
+    		        var source = {};
+    		        var loopsource = {};
+    		        
+    		        source.account = searchResults[iTemp].getText({ name: 'account', summary: search.Summary.GROUP });
+    		        source.type = searchResults[iTemp].getText({ name: 'type', summary: search.Summary.GROUP });
+    		        source.tranid = searchResults[iTemp].getValue({ name: 'tranid', summary: search.Summary.GROUP });
+    		        source.trandate = searchResults[iTemp].getValue({ name: 'trandate', summary: search.Summary.MAX });
+    		        source.mainname = searchResults[iTemp].getText({ name: 'mainname', summary: search.Summary.GROUP }); 
+    		        source.memo = searchResults[iTemp].getValue({ name: 'memo', summary: search.Summary.GROUP });
+    		        source.amount = (searchResults[iTemp].getValue({ name: 'amount', summary: search.Summary.SUM })).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
+    		        loopsource.values = source;
+    		        finalResults.push(loopsource);
+    			}
+    		}
+    		
+    		return finalResults;
+    	}catch(e){
+    		log.debug(e.name, e.message);
+    	}
+    }
+    
     return {
         onRequest: onRequestPrintGPAnalysis
     };
--- CPM_GPAnalysis/CPM_UE_PrintJob_GPAnalysisReport.js
+++ CPM_GPAnalysis/CPM_UE_PrintJob_GPAnalysisReport.js
@@ -22,67 +22,70 @@ function(runtime, url, search, record, runtime) {
      * @Since 2015.2
      */
     function beforeLoadPrintGPAnalysis(sc) {
-    	//Script should work in "VIEW" mode and "UI" contextType
-    	if (sc.type != sc.UserEventType.VIEW && runtime.executionContext != runtime.ContextType.USER_INTERFACE) return;
-    	
-    	//Script Parameters
-    	var scriptObj = runtime.getCurrentScript();
-		var suiteletsid = scriptObj.getParameter({name: 'custscript_pjpue_ssid'});
-		var suiteletdid = scriptObj.getParameter({name: 'custscript_pjpue_sdid'});
-		log.debug('Script Parameters', 'Suitelet SID: '+suiteletsid+' and Suitelet DID: '+suiteletdid);
-		
-    	//Search of 'INVOICE' type to fetch
-    	var searchObj = search.create({
-			type : record.Type.INVOICE,
-			
-			filters : [
-
-				search.createFilter({
-					name 	 : 'custcoljobnbr',
-					operator : 'is',
-					values 	 : sc.newRecord.getValue("id")
-				}), 
-				
-				search.createFilter({
-					name 	 : 'accounttype',
-					operator : 'anyof',
-					values 	 : 'Income'
-				})
-			],
-			
-			columns : [ 
-				search.createColumn({
-					name : 'internalid',
-				}) 
-			]
-		});
-    	
-    	var searchResults = searchObj.run().getRange({
-			start: 0,
-			end : 9
-		});
-    	log.debug('searchResults.length', searchResults.length);
-    	
-    	//load the backend suitelet url if the above search returns the results
-    	if (searchResults.length > 0){ 
-    		var creURL = url.resolveScript({
-                'scriptId'			:suiteletsid,
-                'deploymentId'		:suiteletdid,
-                'returnExternalUrl' : false
-            }) + '&jobid=' + sc.newRecord.getValue("id");
+    	try{
+    		//Script should work in "VIEW" mode and "UI" contextType
+        	if (sc.type != sc.UserEventType.VIEW && runtime.executionContext != runtime.ContextType.USER_INTERFACE) return;
+        	
+        	//Script Parameters
+        	var scriptObj = runtime.getCurrentScript();
+    		var suiteletsid = scriptObj.getParameter({name: 'custscript_pjpue_ssid'});
+    		var suiteletdid = scriptObj.getParameter({name: 'custscript_pjpue_sdid'});
+    		log.debug('Script Parameters', 'Suitelet SID: '+suiteletsid+' and Suitelet DID: '+suiteletdid);
     		
-    		log.debug('sc.newRecord.getValue("id")', sc.newRecord.getValue("id"));
+        	//Search of 'INVOICE' type to fetch
+        	var searchObj = search.create({
+    			type : record.Type.INVOICE,
+    			
+    			filters : [
 
-    		//inline code to work on Button click and to call the above backend suitelet.
-    		var getPrintJobProfitability = "require([], function() { window.open('"+creURL+"','Profitability Report');});";
-    		//var getPrintJobProfitability = "require([], function() { alert('Print Job Development is In-Progress...');});";
-    		
-    		//Adding a custom button to "Print Job (Opportunity)" record before loading the form
-    		sc.form.addButton({
-                id           : 'custpage_button_printjobprofit',
-                label        : 'Profit Report1',
-                functionName : getPrintJobProfitability
-            });
+    				search.createFilter({
+    					name 	 : 'custcoljobnbr',
+    					operator : 'is',
+    					values 	 : sc.newRecord.getValue("id")
+    				}), 
+    				
+    				search.createFilter({
+    					name 	 : 'accounttype',
+    					operator : 'anyof',
+    					values 	 : 'Income'
+    				})
+    			],
+    			
+    			columns : [ 
+    				search.createColumn({
+    					name : 'internalid',
+    				}) 
+    			]
+    		});
+        	
+        	var searchResults = searchObj.run().getRange({
+    			start: 0,
+    			end : 9
+    		});
+        	log.debug('searchResults.length', searchResults.length);
+        	
+        	//load the backend suitelet url if the above search returns the results
+        	if (searchResults.length > 0){ 
+        		var creURL = url.resolveScript({
+                    'scriptId'			:suiteletsid,
+                    'deploymentId'		:suiteletdid,
+                    'returnExternalUrl' : false
+                }) + '&jobid=' + sc.newRecord.getValue("id");
+        		
+        		log.debug('sc.newRecord.getValue("id")', sc.newRecord.getValue("id"));
+
+        		//inline code to work on Button click and to call the above backend suitelet.
+        		var getPrintJobProfitability = "require([], function() { window.open('"+creURL+"','Profitability Report');});";
+        		
+        		//Adding a custom button to "Profit Report" record before loading the form
+        		sc.form.addButton({
+                    id           : 'custpage_button_printjobprofit',
+                    label        : 'Profit Report',
+                    functionName : getPrintJobProfitability
+                });
+        	}
+    	}catch(e){
+    		log.debug(e.name, e.message);
     	}
     }
 
